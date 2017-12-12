df0827f7eb4f67134864d784bf0b2547eaa19f80
twkimskyz <twkim.skyz@gmail.com>
2017-12-12 22:31:22 +0900

    add func_add()

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int func_add ( int arg1, int arg2 )
+{
+	return ( arg1 + arg2 );
+}
+
 int main ()
 {
+	int		arg1;
+	int		arg2;
+	int		sum;
+
 	printf("hello-world\n");
+
+	arg1 = 4;
+	arg2 = 5;
+
+	sum = func_add ( arg1, arg2 );
+
+	printf ("sum = %d\n", sum);
+
 	return 0;
 }
